84c485ac4920dab7ec85ed9b4be9f8848f0bfac0
Benedict <newlyf38@gmail.com>
2023-03-01 08:07:12 +0000

    reacted a generic view class called AttendanceListView in my project

--- myApp/views.py
+++ myApp/views.py
@@ -8,3 +8,13 @@ from django.contrib.admin.views.decorators import staff_member_required
 from datetime import date, timedelta
 from .models import Student, Attendance
 # Create your views here.
+
+
+class AttendanceListView(ListView):
+    model = Attendance
+    template_name = 'attendance_list.html'
+
+    def get_queryset(self):
+        start_date = date.today() - timedelta(days=7)
+        end_date = date.today()
+        return Attendance.objects.filter(date__range=[start_date, end_date]).order_by('-date')
