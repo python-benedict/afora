b7692bb8d0f48a8aca5a7ab4857b44e1dd81fd14
Benedict <newlyf38@gmail.com>
2023-03-01 08:01:54 +0000

    imported generic views

--- myApp/views.py
+++ myApp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
 # Create your views here.
