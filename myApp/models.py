6e9af1fffcb6c87f903b44fd5864ba778a0561ef
Benedict <newlyf38@gmail.com>
2023-03-01 07:58:44 +0000

    created another model called Attendance

--- myApp/models.py
+++ myApp/models.py
@@ -8,3 +8,13 @@ class Student(models.Model):
     
     def __str__(self):
         return self.name
+    
+
+
+class Attendance(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.DateField()
+    present = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.student.name} ({self.date})"
