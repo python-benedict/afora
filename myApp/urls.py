a0c82c2b1f16f95ddd4ddffa7465c72b742eee16
Benedict <newlyf38@gmail.com>
2023-03-01 09:08:25 +0000

    created the attendance_create url

--- myApp/urls.py
+++ myApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.AttendanceListView.as_view(), name='index'), 
     path('<int:pk>/', views.StudentDetailView.as_view(), name='detail'), 
+    path('attendance_create/', views.AttendanceCreateView.as_view(), name='attendance_create'), 
 ]
